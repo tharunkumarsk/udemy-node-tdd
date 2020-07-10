1ba323dd6e7e927d530f3a37abaf15fd7d17d308
Tharunkumar Kanthasamy <>
2020-07-10 13:20:03 +0200

    Feat:Add multiple items and calculate total

--- src/checkout-cart/checkout.js
+++ src/checkout-cart/checkout.js
@@ -1,9 +1,18 @@
 module.exports = class Checkout {
+    constructor(){
+        this.total =0;
+        this.prices = new Object();
+    }
     addItemPrice(item,price){
-
+        this.prices[item] = price;
     }
 
     addItem(item){
+        this.total += this.prices[item]
 
+    }
+
+    calculateTotal(){
+    return this.total;
     }
 };
--- test/checkout_test.js
+++ test/checkout_test.js
@@ -1,4 +1,6 @@
+var expect = require("chai").expect;
 const Checkout = require('../src/checkout-cart/checkout');
+
 var checkout;
 
 beforeEach(function () {
@@ -14,4 +16,18 @@ it('should add an item', function () {
 
     checkout.addItemPrice("bread" ,1);
     checkout.addItem("bread");
+});
+
+it('should calculate total price', function () {
+    checkout.addItemPrice("bread" ,1);
+    checkout.addItem("bread");
+    expect(checkout.calculateTotal()).to.equal(1);
+});
+
+it('should calculate total price for multiple items', function () {
+    checkout.addItemPrice("bread" ,1);
+    checkout.addItemPrice("water" ,2);
+    checkout.addItem("bread");
+    checkout.addItem("water");
+    expect(checkout.calculateTotal()).to.equal(3);
 });
