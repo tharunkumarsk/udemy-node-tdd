773926f1fa9ea739c7fc5058636718023520347b
Tharunkumar Kanthasamy <>
2020-07-10 15:37:18 +0200

    Test:Extract to before each

--- test/checkout_test.js
+++ test/checkout_test.js
@@ -5,20 +5,19 @@ var checkout;
 
 beforeEach(function () {
     checkout = new Checkout()
+    checkout.addItemPrice("bread" ,1);
+    checkout.addItem("bread");
 });
 
 
 
 it('should calculate total price', function () {
-    checkout.addItemPrice("bread" ,1);
-    checkout.addItem("bread");
+
     expect(checkout.calculateTotal()).to.equal(1);
 });
 
 it('should calculate total price for multiple items', function () {
-    checkout.addItemPrice("bread" ,1);
     checkout.addItemPrice("water" ,2);
-    checkout.addItem("bread");
     checkout.addItem("water");
     expect(checkout.calculateTotal()).to.equal(3);
 });
