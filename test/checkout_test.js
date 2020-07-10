7791f0c2fdfc517f1686959e7e33b88fd7c26532
Tharunkumar Kanthasamy <>
2020-07-10 10:00:47 +0200

    Test:Extract to before each

--- test/checkout_test.js
+++ test/checkout_test.js
@@ -1,14 +1,17 @@
 const Checkout = require('../src/checkout-cart/checkout');
+var checkout;
+
+beforeEach(function () {
+    checkout = new Checkout()
+});
 
 
 it('should add item price', function () {
-    var checkout = new Checkout();
     checkout.addItemPrice("bread",1);
 });
 
 it('should add an item', function () {
 
-    var checkout = new Checkout()
     checkout.addItemPrice("bread" ,1);
     checkout.addItem("bread");
 });
